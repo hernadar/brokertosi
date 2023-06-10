b6a61c3eb997ab6201c061083055df53c8240b27
hernadar <dario.hernandez@gmail.com>
2023-06-10 16:12:28 -0300

    Cambio logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import image from '../assets/images/app/LogoBrokerTosi.png';
+import image from '../assets/images/app/LogoTosi2.png';
 import {Link} from 'react-router-dom';
 import '../assets/css/nav.css'
 
